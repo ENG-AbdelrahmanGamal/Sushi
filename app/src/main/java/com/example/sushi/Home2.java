f79d48bfd36dab287a9c3587c77c5810ff8ef17b
Abdelrahman Gamal <abdelrahmanbetter@stud.cu.edu.eg>
2021-05-02 03:05:30 +0200

    finsh menue

--- app/src/main/java/com/example/sushi/Home2.java
+++ app/src/main/java/com/example/sushi/Home2.java
@@ -1,5 +1,6 @@
 package com.example.sushi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,73 +11,121 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.sushi.databinding.ActivityHome2Binding;
+import com.google.android.material.navigation.NavigationView;
 
-public class Home2 extends AppCompatActivity {
+public class Home2 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     ActivityHome2Binding binding;
     DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(Home2.this, R.layout.activity_home2);
+drawerLayout=binding.DrawerLayout;
+navigationView=binding.navigationView;
+toolbar=binding.toolsbar;
+//   *************************** Navigation Drawer ***************************
+        navigationView.bringToFront();
+ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,
+        drawerLayout,toolbar,R.string.navigaton_open,R.string.navigaton_close);
+drawerLayout.addDrawerListener(toggle);
+toggle.syncState();
+navigationView.setNavigationItemSelectedListener(this);
 
-drawerLayout=findViewById(R.id.drawer_layout);
 
         }
-public void ClickMenue(View view)
-{
-    openDrawer(drawerLayout);
-
-}
-
-    private static void openDrawer(DrawerLayout drawerLayout) {
-
-        drawerLayout.openDrawer(GravityCompat.START);
-    }
-    public void clickLogo(View view)
-    {
-        closeDrawer( drawerLayout);
-    }
-
-    private static void closeDrawer(DrawerLayout drawerLayout) {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START))
-            drawerLayout.closeDrawer(GravityCompat.START);
-    }
-    public void clickHome(View view)
-    {
-        recreate();
-    }
-    public void clickDash(View view)
-    {
-        redirectActivity(this,DrawerLayout.class);
-
-    }
-
-    private static void redirectActivity(Activity activity,Class classs) {
-
-        Intent intent=new Intent(activity,classs);
-        activity.startActivity(intent);
-    }
 
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
             drawerLayout.closeDrawer(GravityCompat.START);
-
-        } else {
+        else {
             super.onBackPressed();
         }
     }
+    public void openHome(){
+
+    }
+    public void openOrder(){
+
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.optinal_menue,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.home2_menue_home:
+            {
+                Intent intent=new Intent(Home2.this,Home3.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.home2_menue_order:
+            {
+                Intent intent=new Intent(Home2.this,Order.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.home2_menue_Mnue:
+            {
+                Intent intent=new Intent(Home2.this,Menue.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.home2_menue_restaurant:
+            {
+                Intent intent=new Intent(Home2.this,Restaurant.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.home2_menue_favorites:
+            {
+                Intent intent=new Intent(Home2.this,Favorites.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.home2_menue_myCard:
+            {
+                Intent intent=new Intent(Home2.this,MyCard.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.home2_menue_myAccount:
+            {
+                Intent intent=new Intent(Home2.this,MyAccount.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.home2_menue_aboutUs:
+            {
+                Intent intent=new Intent(Home2.this,AboutUS.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.home2_menue_contactUS:
+            {
+                Intent intent=new Intent(Home2.this,ContactsUs.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.home2_menue_Terms_and_conditions:
+            {
+                Intent intent=new Intent(Home2.this,TermsAndConditions.class);
+                startActivity(intent);
+                break;
+            }
 
+
+        }
+        return true;
     }
+}
---
+++ app/src/main/java/com/example/sushi/MyCard.java
@@ -0,0 +1,14 @@
+package com.example.sushi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_card);
+    }
+}
